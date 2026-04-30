ed5b8554d7c1d4b1a1b2d6409808a2d49b58c978
The Crypto Donkey <cryptofire365@gmail.com>
2026-04-30 23:12:38 +0100

    Deploy Forge Realms production loader fix

--- site/forge-realms/sw.js
+++ site/forge-realms/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "forge-realms-v5";
+const CACHE_NAME = "forge-realms-v6";
 const CORE_ASSETS = ["", "index.html", "manifest.webmanifest", "icon.svg"].map((path) =>
   new URL(path, self.registration.scope).toString(),
 );
