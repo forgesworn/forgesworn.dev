857444251976e49efd62daa961902375d0ade056
The Crypto Donkey <cryptofire365@gmail.com>
2026-05-01 16:04:12 +0100

    Deploy Forge Realms terrain visual pass

--- site/forge-realms/sw.js
+++ site/forge-realms/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "forge-realms-v12";
+const CACHE_NAME = "forge-realms-v13";
 const CORE_ASSETS = ["", "index.html", "manifest.webmanifest", "icon.svg"].map((path) =>
   new URL(path, self.registration.scope).toString(),
 );
