c108e50264474db490440608599c530e5a45e357
The Crypto Donkey <cryptofire365@gmail.com>
2026-05-01 22:15:39 +0100

    Deploy Forge Realms performance checkpoint

--- site/forge-realms/sw.js
+++ site/forge-realms/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "forge-realms-v13";
+const CACHE_NAME = "forge-realms-v14";
 const CORE_ASSETS = ["", "index.html", "manifest.webmanifest", "icon.svg"].map((path) =>
   new URL(path, self.registration.scope).toString(),
 );
