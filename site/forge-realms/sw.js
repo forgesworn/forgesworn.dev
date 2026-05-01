dc91c2ecf51fb1d6904ed10003dc5e63746eba06
The Crypto Donkey <cryptofire365@gmail.com>
2026-05-02 00:39:45 +0100

    Deploy Forge Realms desktop rendering fix

--- site/forge-realms/sw.js
+++ site/forge-realms/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "forge-realms-v14";
+const CACHE_NAME = "forge-realms-v15";
 const CORE_ASSETS = ["", "index.html", "manifest.webmanifest", "icon.svg"].map((path) =>
   new URL(path, self.registration.scope).toString(),
 );
