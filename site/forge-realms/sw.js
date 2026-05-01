90dff035563befa25ab86419e484dc63f99b9118
The Crypto Donkey <cryptofire365@gmail.com>
2026-05-01 13:55:06 +0100

    Deploy Forge Realms mobile stability hotfix

--- site/forge-realms/sw.js
+++ site/forge-realms/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "forge-realms-v7";
+const CACHE_NAME = "forge-realms-v8";
 const CORE_ASSETS = ["", "index.html", "manifest.webmanifest", "icon.svg"].map((path) =>
   new URL(path, self.registration.scope).toString(),
 );
